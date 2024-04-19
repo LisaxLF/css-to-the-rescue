6241342f6f473a85c798941560c79fc0ec73f0d1
LisaxLF <Lisafokkinga@gmail.com>
2024-04-19 21:35:24 +0200

    update media stylequeries

--- js/tinybitofjs.js
+++ js/tinybitofjs.js
@@ -4,12 +4,15 @@ const groteInnerboom = document.getElementById('grote-innerboom');
 const groteOuterboom = document.getElementById('grote-outerboom');
 
 // SLIDERS VOOR DE DIKTE VAN HET VUURWERK
-const dikteInnerboom = document.getElementById('Dikte-innerboom');
-const dikteOuterboom = document.getElementById('Dikte-outerboom');
+const dikteInnerboom = document.getElementById('dikte-innerboom');
+const dikteOuterboom = document.getElementById('dikte-outerboom');
 
 // SLIDERS VOOR DE VERTE VAN HET MIDDEN
-const verteInnerboom = document.getElementById('Verte-innerboom');
-const verteOuterboom = document.getElementById('Verte-outerboom');
+const verteInnerboom = document.getElementById('verte-innerboom');
+const verteOuterboom = document.getElementById('verte-outerboom');
+
+// SLIDERS VOOR DE BORDER VAN DE INNERRING
+const innerringBorder = document.getElementById('dikte-innerring');
 
 // Functie om de waarde van de range sliders in te stellen als custom properties
 function setCustomProperty() {
@@ -26,6 +29,9 @@ function setCustomProperty() {
     const verteInnerboomValue = verteInnerboom.value;
     const verteOuterboomValue = verteOuterboom.value;
 
+    // Sliders voor de dikte van de innerring
+    const innerringBorderValue = innerringBorder.value;
+
     // SLIDERS VOOR DE GROTE VAN HET VUURWERK
     document.documentElement.style.setProperty('--inner-ring-width', `${innerringValue}px`);
     document.documentElement.style.setProperty('--inner-boom-height', `${innerboomValue}px`);
@@ -38,7 +44,9 @@ function setCustomProperty() {
     // SLIDERS VOOR DE VERTE VAN HET MIDDEN
     document.documentElement.style.setProperty('--inner-boom-translateY', `-${verteInnerboomValue}px`);
     document.documentElement.style.setProperty('--outer-boom-translateY', `-${verteOuterboomValue}px`);
-    
+
+    // Sliders voor de dikte van de innerring
+    document.documentElement.style.setProperty('--inner-ring-border-width', `${innerringBorderValue}px`);
 }
 
 // Roep de functie aan wanneer de waarde van de range sliders verandert
@@ -49,6 +57,7 @@ dikteInnerboom.addEventListener('input', setCustomProperty);
 dikteOuterboom.addEventListener('input', setCustomProperty);
 verteInnerboom.addEventListener('input', setCustomProperty);
 verteOuterboom.addEventListener('input', setCustomProperty);
+innerringBorder.addEventListener('input', setCustomProperty);
 
 // Roep de functie aan bij het laden van de pagina om de initiële waarden in te stellen
 setCustomProperty();
